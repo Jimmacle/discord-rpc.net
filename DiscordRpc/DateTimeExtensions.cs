2b35ac32bb154513841e54003e9e5e0ea060b60f
Unknown025 <Unknown025@users.noreply.github.com>
2018-02-10 11:33:42 -0800

    Fixed DLL import errors and Unix time conversion.

--- DiscordRpc/DateTimeExtensions.cs
+++ DiscordRpc/DateTimeExtensions.cs
@@ -6,13 +6,16 @@ namespace DiscordRpc {
         public static DateTime FromUnixTime(this long unixTime)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            return epoch.AddSeconds(unixTime);
+            //return epoch.AddSeconds(unixTime);
+            var timeSpan = TimeSpan.FromSeconds(unixTime);
+            return epoch.Add(timeSpan).ToLocalTime();
         }
 
         public static long ToUnixTime(this DateTime date)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            return Convert.ToInt64((date - epoch).TotalSeconds);
+            //return Convert.ToInt64((date - epoch).TotalSeconds);
+            return (long)(date.ToUniversalTime() - epoch).TotalSeconds;
         }
     }
 }
